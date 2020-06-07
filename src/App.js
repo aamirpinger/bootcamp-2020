aa86981ebbeb272f9fac52d7629fb3a495446f96
Aamir Pinger <muhammad.pinger@trilogy.com>
2020-06-07 23:49:41 +0500

    parent - child code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Dinner from './dinner.js';
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className='App'>
+      {/* <Dinner></Dinner> */}
+      <Dinner dishName='Nihaari' sweetDish='Kheer' />
+      <hr />
+      <Dinner dishName='Biryani' sweetDish='Jaleebi' />
+      <hr />
+      <Dinner dishName='Karahi' sweetDish='Gajar ka Halwa' />
+    </div>
+  );
 }
-
 export default App;
